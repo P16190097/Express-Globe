b3a60476e2eb4684ffbe4852aca9d264665eb1f7
Joshua Mayo <Joshua.Mayo@olmgroup.com>
2021-02-26 11:07:58 +0000

    moved public to docs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
